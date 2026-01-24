71cce16db2928ca853b777d27c25961cda2fad6c
maximum0 <maximum.zero95@gmail.com>
2026-01-25 05:14:46 +0900

    feat: 모니터링을 위한 Actuator 및 Prometheus 설정 추가
    - spring-boot-starter-actuator 의존성 추가
    - Prometheus 메트릭 수집 엔드포인트 설정 추가
    - spring security 모니터링 path 허용

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,6 +67,10 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+    // Monitoring
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
     // Spring REST Docs
     asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
--- src/main/java/com/maximum0/fastpickbe/common/config/SecurityConfig.java
+++ src/main/java/com/maximum0/fastpickbe/common/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/coupons").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/coupons/{id}").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
